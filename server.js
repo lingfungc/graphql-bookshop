93b8f7bbcc02396390a2fed804ca670dbde6af3c
Ling Fung Chan <justinlfchan@gmail.com>
2023-02-02 21:33:18 +0000

    added authors and books seed data

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
 
+const authors = [
+  { id: 1, name: "J. K. Rowling" },
+  { id: 2, name: "J. R. R. Tolkien" },
+  { id: 3, name: "Brent Weeks" },
+];
+
+const books = [
+  { id: 1, name: "Harry Porter and the Chamber of Secrets", authorId: 1 },
+  { id: 2, name: "Harry Porter and the Prisoner of Azkaban", authorId: 1 },
+  { id: 3, name: "Harry Porter and the Goblet of Fire", authorId: 1 },
+  { id: 4, name: "The Fellowship of the Ring", authorId: 2 },
+  { id: 5, name: "The Two Towers", authorId: 2 },
+  { id: 6, name: "The Return of the King", authorId: 2 },
+  { id: 7, name: "The Way of Shadows", authorId: 3 },
+  { id: 8, name: "Beyond the Shadows", authorId: 3 },
+];
+
 app.listen(8000, () => {
   console.log("Running Server.js");
 });
